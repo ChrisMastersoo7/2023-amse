188a13044394e876a153641ebf818ac162a093e7
ChrisMastersoo7 <minuth.christian@gmail.com>
2023-06-15 16:03:51 +0200

    Drop all rows with dash.

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -5,6 +5,9 @@ dataset_url = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251
 columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro','hybrid', 'plugInHybrid', 'others']
 
 df = pd.read_csv(dataset_url, sep=';', encoding="latin1", header=None, skiprows=7, skipfooter=4,
-                 usecols=[*range(0, 3), 12, 22, 32, 42, 52, 62, 72], names=columns, engine='python', converters={'CIN': str})
+                 usecols=[*range(0, 3), 12, 22, 32, 42, 52, 62, 72], names=columns, engine='python',
+                 converters={'CIN': str})
+
+df = df[df["petrol"].str.contains("-")==False]
 
 df.to_sql('cars', 'sqlite:///cars.sqlite', if_exists='replace', index=False)
