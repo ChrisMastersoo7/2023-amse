64727dd7716bd0939ffc0a44b1c1bf6f7f9394fe
ChrisMastersoo7 <minuth.christian@gmail.com>
2023-06-15 15:08:21 +0200

    Add exercise3.

---
+++ exercises/exercise3.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+
+dataset_url = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
+
+columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro','hybrid', 'plugInHybrid', 'others']
+
+df = pd.read_csv(dataset_url, sep=';', encoding="latin1", header=None, skiprows=6, skipfooter=4, usecols=[*range(0, 3), 12, 22, 32, 42, 52, 62, 72], names=columns, engine='python')
+
+df.to_sql('cars', 'sqlite:///cars.sqlite', if_exists='replace', index=False)
