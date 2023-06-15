7babeb8c006bc53a6bc1e34c86e27668d14e19c9
ChrisMastersoo7 <minuth.christian@gmail.com>
2023-06-15 15:45:17 +0200

    Changed 'CIN' field to str to keep leading zero.

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -4,6 +4,7 @@ dataset_url = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251
 
 columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro','hybrid', 'plugInHybrid', 'others']
 
-df = pd.read_csv(dataset_url, sep=';', encoding="latin1", header=None, skiprows=7, skipfooter=4, usecols=[*range(0, 3), 12, 22, 32, 42, 52, 62, 72], names=columns, engine='python')
+df = pd.read_csv(dataset_url, sep=';', encoding="latin1", header=None, skiprows=7, skipfooter=4,
+                 usecols=[*range(0, 3), 12, 22, 32, 42, 52, 62, 72], names=columns, engine='python', converters={'CIN': str})
 
 df.to_sql('cars', 'sqlite:///cars.sqlite', if_exists='replace', index=False)
